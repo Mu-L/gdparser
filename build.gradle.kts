02c29d4ad412ef6383fa0b99abe59eca2e5a95c1
SuperIce <mail@superice.dev>
2026-03-10 23:38:59 +0800

    chore(release): bump version to 0.4.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 val defaultProjectGroup = "dev.superice"
-val defaultProjectVersion = "0.4.2"
+val defaultProjectVersion = "0.4.3"
 val requestedGroup = findProperty("group")?.toString()
 val requestedVersion = findProperty("version")?.toString()
 
